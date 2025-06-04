29f245447918e3c65f2291d717841a79ca0cc0eb
Hyeyeon Kim <132979277+chyoang30@users.noreply.github.com>
2025-06-04 11:30:05 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,14 @@
+// baekjoon 27866
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string s;
+	int i;
+	cin >> s;
+	cin >> i;
+	cout << s[i-1] << endl;
+	return 0;
+}
